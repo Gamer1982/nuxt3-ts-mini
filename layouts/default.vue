2fd8b237b6f106f953f0c66ba241e72a571d7dfa
Igrok82 <pplazy1982@gmail.com>
2023-02-22 09:21:36 +0300

    ref css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="text-lg container flex flex-col p-3 m-3 justify-between bg-slate-400">
-        <header class="bg-green-500 flex text-orange-200 ">
+    <div class="text-lg p-4 m-4  flex flex-col justify-between bg-slate-400">
+        <header class="bg-green-500 flex text-orange-200 p-2">
             <h2>Header</h2>
         </header>
-        <main class="flex bg-purple-700 text-white flex-col justify-around grow">
+        <main class="flex bg-purple-700 text-white flex-col justify-around grow p-2 mt-1 mb-1">
             <slot />
         </main>
-        <footer class="flex justify-between bg-emerald-900 text-cyan-300 shadow">
+        <footer class="flex justify-between bg-emerald-900 text-cyan-300 shadow p-2">
             <NuxtLink to="/">Home</NuxtLink>
             <NuxtLink to="/about">About</NuxtLink>
             <NuxtLink to="/xiaomi">Xiaomi</NuxtLink>
@@ -19,6 +19,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
